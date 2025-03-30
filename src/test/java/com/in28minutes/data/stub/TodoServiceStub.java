0752254493b7df72f9280f17ea9e10a0e8190ba7
Jean-Michel Bodart <jmbodart@skynet.be>
2025-03-30 12:52:38 +0200

    Update TodoServiceStub.java
    Formatting

--- src/test/java/com/in28minutes/data/stub/TodoServiceStub.java
+++ src/test/java/com/in28minutes/data/stub/TodoServiceStub.java
@@ -6,9 +6,13 @@ import java.util.List;
 import com.in28minutes.data.api.TodoService;
 
 public class TodoServiceStub implements TodoService {
+
 	public List<String> retrieveTodos(String user) {
-		return Arrays.asList("Learn Spring MVC", "Learn Spring",
-				"Learn to Dance");
+		return Arrays.asList(
+			"Learn Spring MVC",
+			"Learn Spring",
+			"Learn to Dance"
+			);
 	}
 
 	public void deleteTodo(String todo) {
